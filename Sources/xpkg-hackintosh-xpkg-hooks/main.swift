88e32a8d4ee4c87cae3e9858b5c737ec937b8d12
Sam Deane <sam@elegantchaos.com>
2020-07-01 15:30:03 +0100

    cleaned up

--- Sources/xpkg-hackintosh-xpkg-hooks/main.swift
+++ Sources/xpkg-hackintosh-xpkg-hooks/main.swift
@@ -9,6 +9,5 @@ let links = [
 let arguments = CommandLine.arguments
 let package = InstalledPackage(fromCommandLine: arguments)
 if let action = package.action(fromCommandLine: arguments) {
-    let commands = action == .install ? [["Scripts/setup.sh"]] : []
-    try! package.performAction(fromCommandLine: CommandLine.arguments, links: links, commands: commands)
+    try! package.performAction(fromCommandLine: CommandLine.arguments, links: links, commands: [])
 }
