ea25467e8106bf1303fb1e123da3eefaf1d6139d
Sam Deane <sam@elegantchaos.com>
2020-07-01 15:29:12 +0100

    fixed scripts, aliases

--- Scripts/aliases.sh
+++ Scripts/aliases.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Setup scripts go here"
-alias efi-mount = mount-efi; cd /Volumes/EFI/EFI
+alias efi-mount='mount-efi; cd /Volumes/EFI/EFI'
+alias oc-mount='mount-oc; cd /Volumes/EFI-OC/'
--- Scripts/mount-efi.sh
+++ Scripts/mount-efi.sh
@@ -1,4 +1,10 @@
-sudo diskutil mount disk0s1
+sudo diskutil mount disk1s1
 open /Volumes/EFI/EFI
+open /Volumes/EFI/EFI/config.plist
 cd /Volumes/EFI/EFI/Clover
-stree
+if [[ "$(which fork)" != "" ]]
+then
+    fork
+else
+    stree
+fi
