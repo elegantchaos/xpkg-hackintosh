1913819aa8b3e84e090dbbb9389321a3131af2e4
Sam Deane <sam@elegantchaos.com>
2020-07-01 15:50:54 +0100

    fixed oc path

--- Scripts/aliases.sh
+++ Scripts/aliases.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 alias clover-mount='mount-efi; cd /Volumes/EFI-CLOVER/EFI/Clover'
-alias oc-mount='mount-oc; cd /Volumes/EFI-OC/'
+alias oc-mount='mount-oc; cd /Volumes/EFI-OC/EFI/OC'
